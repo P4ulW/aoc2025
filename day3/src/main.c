e53d2e7a4419ccc1ec0c16e949da20b3cc7f85f0
Paul Werner <paul.werner@uni-goettingen.de>
2025-12-06 23:58:19 +0100

    solved day 4 part 1 and 2

--- day3/src/main.c
+++ day3/src/main.c
@@ -11,196 +11,203 @@ Array_Impl(StringSlice);
 
 const char filename[] = "input.txt";
 
-static void read_file_to_string(String *string, FILE *file) {
-  int c;
-  for (int i = 0; i < string->cap; i++) {
-    c = fgetc(file);
-    if (c == EOF) {
-      break;
+static void read_file_to_string(String *string, FILE *file)
+{
+    int c;
+    for (int i = 0; i < string->cap; i++) {
+        c = fgetc(file);
+        if (c == EOF) {
+            break;
+        }
+
+        // printf("%c", (char)c);
+        String_push(string, (char)c);
+    }
+    return;
+}
+
+static void StringSlice_split_to_slices(
+    ArrayStringSlice *slices, StringSlice to_split, const char split_char)
+{
+    U32 len           = 0;
+    StringSlice slice = {0};
+    int i             = 0;
+    U32 start_new     = 1;
+
+    for (int i = 0; i < to_split.len; i++) {
+        char current = to_split.items[i];
+        if (start_new) {
+            slice.items = &to_split.items[i];
+            len         = 0;
+            slice.len   = 0;
+            start_new   = 0;
+        }
+
+        if (current == split_char) {
+            slice.len = len;
+            ArrayStringSlice_push(slices, slice);
+            start_new = 1;
+        }
+        len += 1;
     }
 
-    // printf("%c", (char)c);
-    String_push(string, (char)c);
-  }
-  return;
+    slice.len = len;
+    // ArrayStringSlice_push(slices, slice);
+
+    return;
 }
 
-static void StringSlice_split_to_slices(ArrayStringSlice *slices,
-                                        StringSlice to_split,
-                                        const char split_char) {
-  U32 len = 0;
-  StringSlice slice = {0};
-  int i = 0;
-  U32 start_new = 1;
+static void StringSlice_print(StringSlice str)
+{
+    printf("str: len %d content: ", str.len);
+    for (int i = 0; i < str.len; i++) {
+        char current = str.items[i];
+        printf("%c", current);
+    }
+    printf("\n");
+    return;
+}
 
-  for (int i = 0; i < to_split.len; i++) {
-    char current = to_split.items[i];
-    if (start_new) {
-      slice.items = &to_split.items[i];
-      len = 0;
-      slice.len = 0;
-      start_new = 0;
+static U64 StringSlice_to_int(StringSlice str)
+{
+    U64 result = 0;
+
+    for (int i = 0; i < str.len; i++) {
+        char current = str.items[i];
+        if ((current < '0') || (current > '9')) {
+            break;
+        }
+        result *= 10;
+        result += current - (int)'0';
     }
 
-    if (current == split_char) {
-      slice.len = len;
-      ArrayStringSlice_push(slices, slice);
-      start_new = 1;
-    }
-    len += 1;
-  }
-
-  slice.len = len;
-  // ArrayStringSlice_push(slices, slice);
-
-  return;
+    return result;
 }
 
-static void StringSlice_print(StringSlice str) {
-  printf("str: len %d content: ", str.len);
-  for (int i = 0; i < str.len; i++) {
-    char current = str.items[i];
-    printf("%c", current);
-  }
-  printf("\n");
-  return;
-}
-
-static U64 StringSlice_to_int(StringSlice str) {
-  U64 result = 0;
-
-  for (int i = 0; i < str.len; i++) {
-    char current = str.items[i];
-    if ((current < '0') || (current > '9')) {
-      break;
-    }
-    result *= 10;
-    result += current - (int)'0';
-  }
-
-  return result;
-}
-
-static inline U32 U32_from_char(char value) {
-  U32 out = 0;
-  if (value < '0') {
-    return out;
-  } else if (value > '9') {
-    return out;
-  }
-
-  return value - '0';
-}
-
-U32 get_bank_joltage(StringSlice bank) {
-  char *left = bank.items;
-  char *right = bank.items;
-  char *current = bank.items;
-  U32 max_left = 0;
-  U32 max_right = 0;
-
-  for (int i = 0; i < bank.len; i++) {
-    U32 current = U32_from_char(*right);
-    // printf("current: %d\n", current);
-    if ((current > max_left) && (i != bank.len - 1)) {
-      max_left = current;
-      max_right = 0;
-    } else if (current > max_right) {
-      max_right = current;
-    }
-    right += 1;
-  }
-
-  printf("left: %u right: %d\n\n", max_left, max_right);
-
-  return max_left * 10 + max_right;
-}
-
-U64 get_bank_joltage_proper(StringSlice bank) {
-  U32 batteries[12] = {0};
-  U64 joltage = 0;
-  char *ptr = bank.items;
-
-  for (int i = 0; i < bank.len; i++) {
-    U32 current = U32_from_char(*ptr);
-
-    if ((current > batteries[0]) && (i < bank.len - 11)) {
-      batteries[0] = current;
-      memset(&batteries[1], 0, 11);
-    } else if ((current > batteries[1]) && (i < bank.len - 10)) {
-      batteries[1] = current;
-      memset(&batteries[2], 0, 10);
-    } else if ((current > batteries[2]) && (i < bank.len - 9)) {
-      batteries[2] = current;
-      memset(&batteries[3], 0, 9);
-    } else if ((current > batteries[3]) && (i < bank.len - 8)) {
-      batteries[3] = current;
-      memset(&batteries[4], 0, 8);
-    } else if ((current > batteries[4]) && (i < bank.len - 7)) {
-      batteries[4] = current;
-      memset(&batteries[5], 0, 7);
-    } else if ((current > batteries[5]) && (i < bank.len - 6)) {
-      batteries[5] = current;
-      memset(&batteries[6], 0, 6);
-    } else if ((current > batteries[6]) && (i < bank.len - 5)) {
-      batteries[6] = current;
-      memset(&batteries[7], 0, 5);
-    } else if ((current > batteries[7]) && (i < bank.len - 4)) {
-      batteries[7] = current;
-      memset(&batteries[8], 0, 4);
-    } else if ((current > batteries[8]) && (i < bank.len - 3)) {
-      batteries[8] = current;
-      memset(&batteries[9], 0, 3);
-    } else if ((current > batteries[9]) && (i < bank.len - 2)) {
-      batteries[9] = current;
-      memset(&batteries[10], 0, 2);
-    } else if ((current > batteries[10]) && (i < bank.len - 1)) {
-      batteries[10] = current;
-      memset(&batteries[11], 0, 1);
-    } else if ((current > batteries[11]) && (i < bank.len)) {
-      batteries[11] = current;
+static inline U32 U32_from_char(char value)
+{
+    U32 out = 0;
+    if (value < '0') {
+        return out;
+    } else if (value > '9') {
+        return out;
     }
 
-    ptr += 1;
-  }
-
-  for (int i = 0; i < 12; i++) {
-    joltage = joltage * 10 + batteries[i];
-  }
-  return joltage;
+    return value - '0';
 }
 
-int main() {
-  clock_t start, end;
-  start = clock();
-  Arena arena = {0};
-  arena_init(&arena, Megabytes(1));
-  String input = String_with_capacity(&arena, Kilobytes(200));
-  ArrayStringSlice banks = ArrayStringSlice_with_capacity(&arena, 1000);
-  FILE *file = fopen(filename, "r");
-  read_file_to_string(&input, file);
-  StringSlice input_as_slice = {.items = input.items, .len = input.len};
-  StringSlice_split_to_slices(&banks, input_as_slice, '\n');
+U32 get_bank_joltage(StringSlice bank)
+{
+    char *left    = bank.items;
+    char *right   = bank.items;
+    char *current = bank.items;
+    U32 max_left  = 0;
+    U32 max_right = 0;
 
-  U64 result_part_1 = 0;
-  U64 result_part_2 = 0;
+    for (int i = 0; i < bank.len; i++) {
+        U32 current = U32_from_char(*right);
+        // printf("current: %d\n", current);
+        if ((current > max_left) && (i != bank.len - 1)) {
+            max_left  = current;
+            max_right = 0;
+        } else if (current > max_right) {
+            max_right = current;
+        }
+        right += 1;
+    }
 
-  for (int i = 0; i < banks.len; i++) {
-    StringSlice bank = ArrayStringSlice_get_value(&banks, i);
-    StringSlice_print(bank);
-    U64 joltage = get_bank_joltage(bank);
-    result_part_1 += joltage;
+    // printf("left: %u right: %d\n\n", max_left, max_right);
 
-    joltage = get_bank_joltage_proper(bank);
-    result_part_2 += joltage;
-  }
+    return max_left * 10 + max_right;
+}
 
-  printf("\x1b[7m\x1b[32mresult part 1: %lu\x1b[0m\n", result_part_1);
-  printf("\x1b[7m\x1b[32mresult part 2: %lu\x1b[0m\n", result_part_2);
-  end = clock();
-  double dt = end - start;
-  printf("time: %f ms\n", dt / 1000.0f);
-  arena_free(&arena);
+U64 get_bank_joltage_proper(StringSlice bank)
+{
+    U32 batteries[12] = {0};
+    U64 joltage       = 0;
+    char *ptr         = bank.items;
 
-  return 0;
+    for (int i = 0; i < bank.len; i++) {
+        U32 current = U32_from_char(*ptr);
+
+        if ((current > batteries[0]) && (i < bank.len - 11)) {
+            batteries[0] = current;
+            memset(&batteries[1], 0, 11);
+        } else if ((current > batteries[1]) && (i < bank.len - 10)) {
+            batteries[1] = current;
+            memset(&batteries[2], 0, 10);
+        } else if ((current > batteries[2]) && (i < bank.len - 9)) {
+            batteries[2] = current;
+            memset(&batteries[3], 0, 9);
+        } else if ((current > batteries[3]) && (i < bank.len - 8)) {
+            batteries[3] = current;
+            memset(&batteries[4], 0, 8);
+        } else if ((current > batteries[4]) && (i < bank.len - 7)) {
+            batteries[4] = current;
+            memset(&batteries[5], 0, 7);
+        } else if ((current > batteries[5]) && (i < bank.len - 6)) {
+            batteries[5] = current;
+            memset(&batteries[6], 0, 6);
+        } else if ((current > batteries[6]) && (i < bank.len - 5)) {
+            batteries[6] = current;
+            memset(&batteries[7], 0, 5);
+        } else if ((current > batteries[7]) && (i < bank.len - 4)) {
+            batteries[7] = current;
+            memset(&batteries[8], 0, 4);
+        } else if ((current > batteries[8]) && (i < bank.len - 3)) {
+            batteries[8] = current;
+            memset(&batteries[9], 0, 3);
+        } else if ((current > batteries[9]) && (i < bank.len - 2)) {
+            batteries[9] = current;
+            memset(&batteries[10], 0, 2);
+        } else if ((current > batteries[10]) && (i < bank.len - 1)) {
+            batteries[10] = current;
+            memset(&batteries[11], 0, 1);
+        } else if ((current > batteries[11]) && (i < bank.len)) {
+            batteries[11] = current;
+        }
+
+        ptr += 1;
+    }
+
+    for (int i = 0; i < 12; i++) {
+        joltage = joltage * 10 + batteries[i];
+    }
+    return joltage;
+}
+
+int main()
+{
+    clock_t start, end;
+    start       = clock();
+    Arena arena = {0};
+    arena_init(&arena, Megabytes(1));
+    String input           = String_with_capacity(&arena, Kilobytes(200));
+    ArrayStringSlice banks = ArrayStringSlice_with_capacity(&arena, 1000);
+    FILE *file             = fopen(filename, "r");
+    read_file_to_string(&input, file);
+    StringSlice input_as_slice = {.items = input.items, .len = input.len};
+    StringSlice_split_to_slices(&banks, input_as_slice, '\n');
+
+    U64 result_part_1 = 0;
+    U64 result_part_2 = 0;
+
+    for (int i = 0; i < banks.len; i++) {
+        StringSlice bank = ArrayStringSlice_get_value(&banks, i);
+        // StringSlice_print(bank);
+        U64 joltage = get_bank_joltage(bank);
+        result_part_1 += joltage;
+
+        joltage = get_bank_joltage_proper(bank);
+        result_part_2 += joltage;
+    }
+
+    printf("\x1b[7m\x1b[32mresult part 1: %lu\x1b[0m\n", result_part_1);
+    printf("\x1b[7m\x1b[32mresult part 2: %lu\x1b[0m\n", result_part_2);
+    end       = clock();
+    double dt = end - start;
+    printf("time: %f ms\n", dt / 1000.0f);
+    arena_free(&arena);
+
+    return 0;
 }
--- day4/src/main.c
+++ day4/src/main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
 #include "arena.c"
 #include "array.c"
 #include "string.c"
+#include <unistd.h>
 typedef struct timeval timeval;
 
 Array_Prototype(StringSlice);
 Array_Impl(StringSlice);
 
-const char filename[] = "test.txt";
+const char filename[] = "input.txt";
 
+typedef enum {
+    Up,
+    Down,
+    Left,
+    Right,
+    LeftUp,
+    RightUp,
+    LeftDown,
+    RightDown,
+} Direction;
+
+Direction dirs[] = {
+    Up, Down, Left, Right, LeftUp, RightUp, LeftDown, RightDown};
+
+// -------------------------------------------- //
 static void read_file_to_string(String *string, FILE *file)
 {
     int c;
@@ -26,6 +41,7 @@ static void read_file_to_string(String *string, FILE *file)
     return;
 }
 
+// -------------------------------------------- //
 static void StringSlice_split_to_slices(
     ArrayStringSlice *slices, StringSlice to_split, const char split_char)
 {
@@ -57,6 +73,7 @@ static void StringSlice_split_to_slices(
     return;
 }
 
+// -------------------------------------------- //
 static void StringSlice_print(StringSlice str)
 {
     printf("str: len %d content: ", str.len);
@@ -68,6 +85,7 @@ static void StringSlice_print(StringSlice str)
     return;
 }
 
+// -------------------------------------------- //
 static U64 StringSlice_to_int(StringSlice str)
 {
     U64 result = 0;
@@ -84,6 +102,7 @@ static U64 StringSlice_to_int(StringSlice str)
     return result;
 }
 
+// -------------------------------------------- //
 static inline U32 U32_from_char(char value)
 {
     U32 out = 0;
@@ -96,8 +115,18 @@ static inline U32 U32_from_char(char value)
     return value - '0';
 }
 
-U32 map_get_width(String)
+// -------------------------------------------- //
+U32 map_get_width(String input)
 {
+    U32 width = 0;
+    for (int i = 0; i < input.len; i++) {
+        char current = input.items[i];
+        if (current == '\n') {
+            break;
+        }
+        width += 1;
+    }
+    return width;
 }
 
 typedef struct {
@@ -106,8 +135,191 @@ typedef struct {
     char *items;
 } Map;
 
+// -------------------------------------------- //
+Map Map_from_string(Arena *arena, String input)
+{
+    Map map     = {0};
+    map.width   = map_get_width(input);
+    map.items   = arena_alloc(arena, input.len);
+    U32 height  = 0;
+    U32 map_idx = 0;
+    for (int i = 0; i < input.len; i++) {
+        char current = input.items[i];
+        if (current == '\n') {
+            height += 1;
+            continue;
+        }
+
+        map.items[map_idx] = current;
+        map_idx += 1;
+    }
+
+    map.height = height;
+
+    return map;
+}
+
+// -------------------------------------------- //
+void Map_print(const Map map)
+{
+    for (int y = 0; y < map.width; y++) {
+        for (int x = 0; x < map.height; x++) {
+            U32 index    = x + y * map.width;
+            char current = map.items[index];
+            printf("%c", current);
+        }
+        printf("\n");
+    }
+}
+
+// -------------------------------------------- //
+U32 Map_check_if_inside(Map map, I32 index)
+{
+    if (index < 0) {
+        return 0;
+    } else if (index >= map.height * map.width) {
+        return 0;
+    }
+
+    return 1;
+}
+
+// -------------------------------------------- //
+U32 Map_get_neighbor_count(Map map, I32 tile_index)
+{
+    U32 count         = 0;
+    U32 is_edge_left  = (tile_index % map.width == 0) ? 1 : 0;
+    U32 is_edge_right = (tile_index % map.width == map.width - 1) ? 1 : 0;
+    U32 is_edge_up    = (tile_index / map.width == 0) ? 1 : 0;
+    U32 is_edge_down  = (tile_index / map.width == map.height - 1) ? 1 : 0;
+
+    // printf(
+    //     "tile_index: %d , left? %u, right? %u, up? %u, down? %u\n",
+    //     tile_index,
+    //     is_edge_left,
+    //     is_edge_right,
+    //     is_edge_up,
+    //     is_edge_down);
+
+    for (int i = 0; i < 8; i++) {
+        Direction dir      = dirs[i];
+        I32 tile_index_new = tile_index;
+
+        switch (dir) {
+            case Left:
+                if (is_edge_left) {
+                    continue;
+                }
+                tile_index_new -= 1;
+                break;
+            case Right:
+                if (is_edge_right) {
+                    continue;
+                }
+                tile_index_new += 1;
+                break;
+            case Up:
+                if (is_edge_up) {
+                    continue;
+                }
+                tile_index_new -= map.width;
+                break;
+            case Down:
+                if (is_edge_down) {
+                    continue;
+                }
+                tile_index_new += map.width;
+                break;
+            case LeftDown:
+                if (is_edge_down || is_edge_left) {
+                    continue;
+                }
+                tile_index_new += map.width;
+                tile_index_new -= 1;
+                break;
+            case LeftUp:
+                if (is_edge_up || is_edge_left) {
+                    continue;
+                }
+                tile_index_new -= map.width;
+                tile_index_new -= 1;
+                break;
+            case RightDown:
+                if (is_edge_down || is_edge_right) {
+                    continue;
+                }
+                tile_index_new += map.width;
+                tile_index_new += 1;
+                break;
+            case RightUp:
+                if (is_edge_up || is_edge_right) {
+                    continue;
+                }
+                tile_index_new -= map.width;
+                tile_index_new += 1;
+                break;
+        }
+
+        // printf("tile_index_new: %d\n", tile_index_new);
+
+        U32 inside = Map_check_if_inside(map, tile_index_new);
+        if (!inside) {
+            continue;
+        }
+
+        char tile = map.items[tile_index_new];
+
+        if (tile != '.') {
+            count += 1;
+        }
+    }
+
+    char tile = map.items[tile_index];
+    if (count < 4 && tile == '@') {
+        tile = 'x';
+    }
+
+    // if (is_edge_right) {
+    //     printf("\x1b[31m%c", tile);
+    // } else if (is_edge_left) {
+    //     printf("\x1b[32m%c", tile);
+    // } else if (is_edge_up) {
+    //     printf("\x1b[33m%c", tile);
+    // } else if (is_edge_down) {
+    //     printf("\x1b[34m%c", tile);
+    // } else {
+    //     printf("\x1b[0m%c", tile);
+    // }
+    //
+    // if (is_edge_right) {
+    //     printf("\n");
+    // }
+
+    if (tile == '.') {
+        return UINT32_MAX;
+    }
+
+    // printf("count: %u\n\n", count);
+
+    return count;
+}
+
+// -------------------------------------------- //
+void Map_remove_loose_paper(Map map)
+{
+    for (I32 i = 0; i < map.width * map.height; i++) {
+        char tile = map.items[i];
+        if (tile == 'x') {
+            map.items[i] = '.';
+        }
+    }
+}
+
+// -------------------------------------------- //
 int main()
 {
+    U64 result_part_1 = 0;
+    U64 result_part_2 = 0;
     clock_t start, end;
     start       = clock();
     Arena arena = {0};
@@ -118,9 +330,40 @@ int main()
 
     U32 width = map_get_width(input);
     Map map   = Map_from_string(&arena, input);
+    // Map_print(map);
 
-    U64 result_part_1 = 0;
-    U64 result_part_2 = 0;
+    for (I32 i = 0; i < map.width * map.height; i++) {
+        U32 neighbors = 0;
+        char tile     = map.items[i];
+
+        neighbors = Map_get_neighbor_count(map, i);
+        if (neighbors < 4) {
+            map.items[i] = 'x';
+            result_part_1 += 1;
+        }
+    }
+
+    Map_remove_loose_paper(map);
+
+    U64 result_prev = 0;
+    result_part_2   = result_part_1;
+
+    while (result_part_2 != result_prev) {
+        result_prev = result_part_2;
+
+        // printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        for (I32 i = 0; i < map.width * map.height; i++) {
+            U32 neighbors = 0;
+            char tile     = map.items[i];
+
+            neighbors = Map_get_neighbor_count(map, i);
+            if (neighbors < 4) {
+                map.items[i] = 'x';
+                result_part_2 += 1;
+            }
+        }
+        Map_remove_loose_paper(map);
+    }
 
     printf("\x1b[7m\x1b[32mresult part 1: %lu\x1b[0m\n", result_part_1);
     printf("\x1b[7m\x1b[32mresult part 2: %lu\x1b[0m\n", result_part_2);
