6d8cc1dd84b8ace4edb38788e792177356572f96
Paul Werner <paul.werner@uni-goettingen.de>
2025-12-07 17:22:18 +0100

    improved new day script

--- make_day.sh
+++ make_day.sh
@@ -1,8 +1,23 @@
 #! /usr/bin/env bash
 
-mkdir -v -p day_new 
-pushd day_new 
+dirname="day"$1
+if [ -d "$dirname" ]; then
+    echo "WARNING: $dirname alreay exists, not creating new one!"
+    exit -1
+fi
+
+mkdir -v -p $dirname 
+pushd $dirname
 mkdir -v src
-echo "#include<stdio.h>\nint main() {\n  return;\n}" >> src/main.c
-echo "default:\ngcc -g -O3 src/main.c -o out" >> Makefile
+pushd src
+git clone git@github.com:P4ulW/cbase.git
+pushd cbase
+git checkout dev
+rm .git -rvf
 popd
+popd
+echo $'#include<stdio.h>\nint main() {\n\treturn;\n}' >> src/main.c
+echo $'default:\n\tgcc -g -O3 src/main.c -o out' >> Makefile
+popd
+echo "$dirname successfully created"
+exit 0
